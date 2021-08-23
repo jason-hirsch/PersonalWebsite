ede9b2fe56d6898779199e782f009d88c77c0f7a
Jason Hirsch <jasonhi10@aol.com>
2021-08-23 00:06:54 -0500

    added mobile image support

--- src/components/homePage/showcase.jsx
+++ src/components/homePage/showcase.jsx
@@ -1,68 +1,35 @@
 import React, { Component } from "react";
 import Resizer from "react-image-file-resizer";
 import "../../../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import "../../styles/showcase.css";
 
 class Showcase extends Component {
     state = {
         image: "",
     };
 
-    imgDivRef = undefined;
-
     constructor(props) {
         super(props);
         this.state.image = this.props.image;
-        this.imgDivRef = React.createRef();
     }
-
-    componentDidMount() {
-        //this.resizeImage();
-        //window.addEventListener("resize", this.resizeImage);
-    }
-
-    componentWillUnmount() {
-        //window.removeEventListener("resize", this.resizeImage);
-    }
-
-    resizeImage = () => {
-        fetch(this.props.image)
-            .then((response) => response.blob())
-            .then((image) =>
-                Resizer.imageFileResizer(
-                    image,
-                    this.imgDivRef.current.offsetWidth * 0.7,
-                    this.imgDivRef.current.offsetHeight * 0.7,
-                    "JPEG",
-                    100,
-                    0,
-                    (uri) => this.setState({ image: uri })
-                )
-            );
-    };
 
     render() {
-        /*
-        return (
-            <div className="container-fluid" style={{ height: "100vh" }}></div>
-        );
-        */
-
         return (
             <div
-                className="d-flex flex-row justify-content-center"
+                className="showcase-container d-flex justify-content-between"
                 style={{ height: "100vh" }}
             >
-                <div className="d-inline d-flex flex-column justify-content-center px-5">
-                    <h1 className="display-4 bg-dark text-warning px-5 py-3">
+                <div className="d-flex flex-column justify-content-center px-5 py-5">
+                    <h1 className="display-4 bg-dark text-warning py-3">
                         {this.props.title}
                     </h1>
-                    <p className="description-text px-5 py-1">
+                    <p className="description-text py-1">
                         {this.props.description}
                     </p>
-                    <p className="technology-text px-5 py-1">
+                    <p className="technology-text py-1">
                         {this.props.technologies.join(", ")}
                     </p>
-                    <div className="d-flex flex-row px-5">
+                    <div className="d-flex flex-row">
                         <a
                             key="fab fa-github"
                             role="button"
@@ -77,17 +44,12 @@ class Showcase extends Component {
                     </div>
                 </div>
 
-                <div className="d-inline text-center px-5" ref={this.imgDivRef}>
-                    <div
-                        className="d-flex flex-column justify-content-center px-5"
-                        style={{ height: "100%" }}
-                    >
-                        <img
-                            className="img-fluid mx-auto py-1"
-                            src={this.state.image}
-                            alt=""
-                        ></img>
-                    </div>
+                <div className="d-flex flex-column justify-content-center px-5 py-5">
+                    <img
+                        className="img-fluid mx-auto"
+                        src={this.state.image}
+                        alt=""
+                    ></img>
                 </div>
             </div>
         );
